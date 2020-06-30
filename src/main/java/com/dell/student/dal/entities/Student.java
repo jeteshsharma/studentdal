1942c13368eb12398a2f4b45236fc866ff117890
Jetesh Sharma <sharma.jetesh1@gmail.com>
2020-06-30 19:35:29 +0530

    backup commit

--- src/main/java/com/dell/student/dal/entities/Student.java
+++ src/main/java/com/dell/student/dal/entities/Student.java
@@ -11,56 +11,55 @@ import javax.persistence.Table;
 @Table(name = "studenttab")
 public class Student {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-	@Column(name = "sname")
-	private String name;
+    @Column(name = "sname")
+    private String name;
 
-	@Column(name = "scourse")
-	private String course;
+    @Column(name = "scourse")
+    private String course;
 
-	@Column(name = "sfee")
-	private Double fee;
+    @Column(name = "sfee")
+    private Double fee;
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getCourse() {
-		return course;
-	}
+    public String getCourse() {
+        return course;
+    }
 
-	public void setCourse(String course) {
-		this.course = course;
-	}
+    public void setCourse(String course) {
+        this.course = course;
+    }
 
-	
 
-	@Override
-	public String toString() {
-		return "Student [id=" + id + ",name="+name +", course=" + course + ", fee=" + getFee() + "]";
-	}
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ",name=" + name + ", course=" + course + ", fee=" + getFee() + "]";
+    }
 
-	public Double getFee() {
-		return fee;
-	}
+    public Double getFee() {
+        return fee;
+    }
 
-	public void setFee(Double fee) {
-		this.fee = fee;
-	}
+    public void setFee(Double fee) {
+        this.fee = fee;
+    }
 
 }
